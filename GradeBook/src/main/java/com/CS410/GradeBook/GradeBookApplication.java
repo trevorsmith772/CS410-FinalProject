99f25b4d8489c7924ac19d6dac8c5b2b99dfb994
Berto Cisneros <bertocisneros@u.boisestate.edu>
2022-04-26 14:06:49 -0600

    adding maven project directory

---
+++ GradeBook/src/main/java/com/CS410/GradeBook/GradeBookApplication.java
@@ -0,0 +1,13 @@
+package com.CS410.GradeBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeBookApplication.class, args);
+	}
+
+}
---
+++ GradeBook/src/test/java/com/CS410/GradeBook/GradeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CS410.GradeBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

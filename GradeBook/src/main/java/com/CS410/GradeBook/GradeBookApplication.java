0c973e745dee3fdbe9f8e6baba8fc919215cff97
Berto Cisneros <bertocisneros@u.boisestate.edu>
2022-04-26 22:49:51 -0600

    Adding query

--- GradeBook/src/main/java/com/CS410/GradeBook/GradeBookApplication.java
+++ GradeBook/src/main/java/com/CS410/GradeBook/GradeBookApplication.java
@@ -11,47 +11,58 @@ import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class GradeBookApplication {
-	public static Connection makeConnection() {
-		try {
-			Connection conn = null;
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:54051/test?verifyServerCertificate=false&useSSL=true", "msandbox",
-					"dbpass30");
-			// Do something with the Connection
-			System.out.println("Database [test db] connection succeeded!");
-			System.out.println();
-			return conn;
-		} catch (SQLException ex) {
-			// handle any errors
-			System.err.println("SQLException: " + ex.getMessage());
-			System.err.println("SQLState: " + ex.getSQLState());
-			System.err.println("VendorError: " + ex.getErrorCode());
-		}
-		return null;
-	}
+//	public static Connection makeConnection() {
+//		try {
+//			Connection conn = null;
+//			conn = DriverManager.getConnection(
+//					"jdbc:mysql://localhost:54051/test?verifyServerCertificate=false&useSSL=true", "msandbox",
+//					"dbpass30");
+//			// Do something with the Connection
+//			System.out.println("Database [test db] connection succeeded!");
+//			System.out.println();
+//			return conn;
+//		} catch (SQLException ex) {
+//			// handle any errors
+//			System.err.println("SQLException: " + ex.getMessage());
+//			System.err.println("SQLState: " + ex.getSQLState());
+//			System.err.println("VendorError: " + ex.getErrorCode());
+//		}
+//		return null;
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradeBookApplication.class, args);
 
-		try {
-			// The newInstance() call is a work around for some broken Java implementations
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			System.out.println();
-			System.out.println("JDBC driver loaded");
-			System.out.println();
-
-			Connection conn = makeConnection();
-			// runQuery(conn);
-
-			conn.close();
-			System.out.println();
-			System.out.println("Database [test db] connection closed");
-			System.out.println();
-		} catch (Exception ex) {
-			// handle the error
-			System.err.println(ex);
-		}
+//		try {
+//			// The newInstance() call is a work around for some broken Java implementations
+//			Class.forName("com.mysql.jdbc.Driver").newInstance();
+//			System.out.println();
+//			System.out.println("JDBC driver loaded");
+//			System.out.println();
+//
+//			Connection conn = makeConnection();
+//			// runQuery(conn);
+//
+//			conn.close();
+//			System.out.println();
+//			System.out.println("Database [test db] connection closed");
+//			System.out.println();
+//		} catch (Exception ex) {
+//			// handle the error
+//			System.err.println(ex);
+//		}
 	}
+	@Autowired
+	JdbcTemplate jdbc;
+
+	public void run(String... strings) throws Exception {
+		jdbc.execute("INSERT INTO classes (class_id) VALUES ('1') " +
+				"course_number = 'CS410', " +
+				"term = 'SP22', " +
+				"section_number = '001', "+
+				"description = 'Software Engineering', ");
+	}
+
 
 //	@Autowired
 //	JdbcTemplate jdbcTemplate;
